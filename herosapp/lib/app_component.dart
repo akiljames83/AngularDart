0be1cc31b84452e0288f9687f1e6f2bb8b5542ff
Akil Hamilton <akil.james83@gmail.com>
2019-03-22 13:12:58 -0400

    Update app_component

--- herosapp/lib/app_component.dart
+++ herosapp/lib/app_component.dart
@@ -1,9 +1,31 @@
 import 'package:angular/angular.dart';
+import 'hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+  		<h1>
+  			Hello {{name}}
+  		</h1>
+  		<p>
+  			Your hero is {{hero.name}}.
+  			<br>
+  			{{hero.name}}'s ID is {{hero.id}}.
+  		</p>
+  		<h2>Update Hero Name:</h2>
+  		<div>
+  			<label>
+  				Update:
+  			</label>
+  			<input [(ngModel)]="hero.name" placeholder="Update name">
+  		</div>
+  		<h2>Details</h2>
+  		<br>
+  		<div><label>id: </label>{{hero.id}}</div>
+  		<div><label>name: </label>{{hero.name}}</div>
+  		''',
 )
 class AppComponent {
   var name = 'Angular';
+  Hero hero = Hero(1, 'Akil');
 }
