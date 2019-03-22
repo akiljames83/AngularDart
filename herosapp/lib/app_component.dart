657bc2bfa15970ab73c83a8d41d4219416216bba
Akil Hamilton <akil.james83@gmail.com>
2019-03-22 14:01:33 -0400

    Version3 edits to heros app

--- herosapp/lib/app_component.dart
+++ herosapp/lib/app_component.dart
@@ -4,28 +4,9 @@ import 'hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-  		<h1>
-  			Hello {{name}}
-  		</h1>
-  		<p>
-  			Your hero is {{hero.name}}.
-  			<br>
-  			{{hero.name}}'s ID is {{hero.id}}.
-  		</p>
-  		<h2>Update Hero Name:</h2>
-  		<div>
-  			<label>
-  				Update:
-  			</label>
-  			<input [(ngModel)]="hero.name" placeholder="Update name">
-  		</div>
-  		<h2>Details</h2>
-  		<br>
-  		<div><label>id: </label>{{hero.id}}</div>
-  		<div><label>name: </label>{{hero.name}}</div>
-  		''',
-  	directives: [formDirectives],
+  //template: , // Replace template with templateUrl
+  templateUrl: 'app_component.html',
+  directives: [formDirectives],
 )
 class AppComponent {
   var name = 'Angular';
