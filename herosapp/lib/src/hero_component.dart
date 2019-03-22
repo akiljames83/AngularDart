e5ffcc0272180bdad3e9ae80e7725da9db8f0b04
Akil Hamilton <akil.james83@gmail.com>
2019-03-22 17:05:46 -0400

    Finish hero_componenet

--- herosapp/lib/src/hero_component.dart
+++ herosapp/lib/src/hero_component.dart
@@ -1,11 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
+import 'hero.dart';
+
 @Component(
 	selector: 'my-hero',
 	templateUrl: ,
 	directives: [formDirectives, coreDirectives]
 )
 class HeroComponent{
-
+	@Input() // Property binding expression
+	Hero hero;
 }
