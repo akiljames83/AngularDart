50911fd1bf3bd93d3864ebb2a927b3b1f36a7521
Akil Hamilton <akil.james83@gmail.com>
2019-03-22 16:59:12 -0400

    Edit hero_component files

--- herosapp/lib/src/hero_component.dart
+++ herosapp/lib/src/hero_component.dart
@@ -3,8 +3,9 @@ import 'package:angular_forms/angular_forms.dart';
 
 @Component(
 	selector: 'my-hero',
+	templateUrl: ,
 	directives: [formDirectives, coreDirectives]
 )
 class HeroComponent{
-	
+
 }
