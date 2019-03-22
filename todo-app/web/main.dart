52d54c091e228df55aafe0456c046c2080540310
Akil Hamilton <akil.james83@gmail.com>
2019-03-22 01:47:41 -0400

    Finish up todo app

--- todo-app/web/main.dart
+++ todo-app/web/main.dart
@@ -2,16 +2,26 @@ import 'dart:html';
 
 InputElement input;
 UListElement list;
+ButtonElement delete;
 
 void main() {
   input = querySelector("#todo-input");
   list = querySelector("#todo-list");
   input.onChange.listen(addTodoItem);
+
+  delete = querySelector("#delete");
+  delete.onClick.listen(
+  	(Event e) => list.children.clear()
+  );
+
 }
 
 void addTodoItem(Event e) {
-	var newToDo = new LIElement();
-	newToDo.text = input.value;
+	// Use .. syntax to refer to current element attributes
+	var newToDo = new LIElement()
+		..text = input.value;
+	newToDo.onClick.listen((Event e) => newToDo.remove());
+
 	input.value = "";
 	list.children.add(newToDo);
 }
