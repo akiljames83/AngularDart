668cb4cb058e21a6b93fc0fced86eaa31c82774a
Akil Hamilton <akil.james83@gmail.com>
2019-03-23 00:50:49 -0400

    Setup for service

--- herosapp/lib/app_component.dart
+++ herosapp/lib/app_component.dart
@@ -4,16 +4,26 @@ import 'package:angular_forms/angular_forms.dart';
 import 'src/hero.dart';
 import 'src/mockHeros.dart';
 import 'src/hero_component.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
   directives: [coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements onInit {
   var title = 'Mock Heros Project';
+
   Hero selected;
-  List<Hero> heros = mockHeros;
   void onClick(hero) => selected = hero;
+
+  final HeroService _heroService;
+  AppComponent(this._heroService);
+
+  List<Hero> heros;
+  void _getHeros() => heros = _heroService.getAll();
+
+  void ngOnInit() => _getHeros();
 }
---
+++ herosapp/lib/src/hero_service.dart
@@ -0,0 +1,6 @@
+import 'hero.dart';
+import 'mockHeros.dart';
+
+class HeroService{
+	List<Hero> getAll() => mockHeros;
+}
