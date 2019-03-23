8d8aa80a564e62e9328f06a120d8025225a3f6ea
Akil Hamilton <akil.james83@gmail.com>
2019-03-23 02:23:19 -0400

    Edits for the routes

--- herosapp/lib/app_component.dart
+++ herosapp/lib/app_component.dart
@@ -4,12 +4,15 @@ import 'package:angular_router/angular_router.dart';
 import 'src/hero_service.dart';
 import 'src/hero_list_component.dart';
 
+import 'src/routes.dart';
+
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [HeroListComponent],
+  directives: [routerDirectives],
   providers: [ClassProvider(HeroService)],
+  exports: [RoutePaths, Routes],
 )
 class AppComponent {
   final title = 'Mock Heros Project';
