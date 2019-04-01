5524ccc53957be4d5ced8de01bd8235c1e48aa66
Akil Hamilton <akil.james83@gmail.com>
2019-04-01 12:51:52 -0400

    Many changes to app creating the dashboard

--- herosapp/lib/src/route_paths.dart
+++ herosapp/lib/src/route_paths.dart
@@ -2,4 +2,5 @@ import 'package:angular_router/angular_router.dart';
 
 class RoutePaths {
   static final heros = RoutePath(path: 'heros');
+  static final dashboard = RoutePath(path: 'dashboard');
 }
